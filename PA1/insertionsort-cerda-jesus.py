94cbd9f7d93892e3edf4069fc78923d3cc940778
JesusFranC <94206855+JesusFranC@users.noreply.github.com>
2023-03-10 19:00:46 -0800

    Update insertionsort-cerda-jesus.py
    I had to untabify the program, because the program was throwing up errors (mix of tabs and spaces)

--- PA1/insertionsort-cerda-jesus.py
+++ PA1/insertionsort-cerda-jesus.py
@@ -22,52 +22,53 @@ import random
 import time
 
 class Solution:
-	
-	# this function returns a descending sorted array.
-	def function_a (self, elements_count: int) -> list:
-		output = []
-		for i in range(elements_count,0, -1):
-			output.append(i)
-		return output
+    
+    # this function returns a descending sorted array.
+    def function_a (self, elements_count: int) -> list:
+        output = []
+        for i in range(elements_count,0, -1):
+            output.append(i)
+        return output
 
-	# this function returns an ascending sorted array.	
-	def function_b (self, elements_count: int) -> list:
-		output = []
-		for i in range(1, elements_count):
-			output.append(i)
-		return output
+    # this function returns an ascending sorted array.  
+    def function_b (self, elements_count: int) -> list:
+        output = []
+        for i in range(1, elements_count):
+            output.append(i)
+        return output
 
-	# this function returns a randomly generated array	
-	def function_c(self, elements_count: int, seed: int):
-		output = []
-		random.seed(seed)
-		for i in range(0,elements_count+1):
-			output.append(random.randint(1,1000000))
+    # this function returns a randomly generated array  
+    def function_c(self, elements_count: int, seed: int):
+        output = []
+        random.seed(seed)
+        for i in range(0,elements_count+1):
+            output.append(random.randint(1,1000000))
 
-		return output
+        return output
 
 
-	def select_input(self, input_type: str, elements_count: int, seed: int) -> list:
-		output = []
-		if input_type == "a":
-			output = self.function_a(elements_count)
-		if 	input_type == "b":
-			output = self.function_b(elements_count)
-		if 	input_type == "c":
-			output = self.function_c(elements_count, seed)
-		return output	
+    def select_input(self, input_type: str, elements_count: int, seed: int) -> list:
+        output = []
+        if input_type == "a":
+            output = self.function_a(elements_count)
+        if  input_type == "b":
+            output = self.function_b(elements_count)
+        if  input_type == "c":
+            output = self.function_c(elements_count, seed)
+        return output   
 
-	def pa1_insertionsort (self, input_type: str, elements_count: int, seed: int) -> list:
-		output = []
-		query_list = self.select_input(input_type, elements_count, seed)
-		
-		n = len(query_list)
+    def pa1_insertionsort (self, input_type: str, elements_count: int, seed: int) -> list:
+                output = []
+                query_list = self.select_input(input_type, elements_count, seed)
+        
+                n = len(query_list)
 
-		# get the start time
-		st = time.process_time()
-		
-    	# your insertion sort algorithm comes here ...
-                if !(len(A) == 0 or len(A) == 1):
+                # get the start time
+                st = time.process_time()
+        
+        # your insertion sort algorithm comes here ...
+                A = query_list
+                if not (len(A) == 0 or len(A) == 1):
                     for i in range(0, len(A)):
                         key = A[i]
                         j = i - 1
@@ -75,33 +76,34 @@ class Solution:
                             A[j+1] = A[j]
                             j = j - 1
                         A[j+1] = key
+                output = A
 
 
-    	# end of insertion sort
-		
-		et = time.process_time()
-		res = et - st
+        # end of insertion sort
+        
+                et = time.process_time()
+                res = et - st
 
-		return [query_list, res]
+                return [query_list, res]
 
 
 
 
 if __name__ == '__main__':
 
-	# the input type is either a, b or c 
-	# corresponding to function_a, function_b and functin_c.
-	input_type = sys.argv[1]
+    # the input type is either a, b or c 
+    # corresponding to function_a, function_b and functin_c.
+    input_type = sys.argv[1]
 
-	elements_count = int(sys.argv[2])
+    elements_count = int(sys.argv[2])
 
-	# input seed as 2, so we have the same randomly 
-	# generated array.
-	# you can change it for your testing.
-	seed = sys.argv[3]
-	
-	obj = Solution()
-	# the return value is an array of array.
-	ret = obj.pa1_insertionsort(input_type, elements_count, seed)
-	print(ret)
+    # input seed as 2, so we have the same randomly 
+    # generated array.
+    # you can change it for your testing.
+    seed = sys.argv[3]
+    
+    obj = Solution()
+    # the return value is an array of array.
+    ret = obj.pa1_insertionsort(input_type, elements_count, seed)
+    print(ret)
 
