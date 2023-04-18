a8fcb0a78492fb2da850b0969714eab819835aea
JesusFranC <jesusfranciscocerda@gmail.com>
2023-04-17 19:43:25 -0700

    Past Half Way
    Past the Half way point. The quicksort variant has been tested and fixed. The Heap variant version is being worked on

--- PA2/MinHeapsort.py
+++ PA2/MinHeapsort.py
@@ -4,9 +4,23 @@ studentID: 029148637
 
 assignment:PA2
 '''
+import math
 
-def BuildMinHeap():
+"""
+Min heapify will ensure the heap condition is met, is the style of a minHeap
+The smaller of the two child nodes will rise to the top, and it will then heapify the affected child node
+
+arr:    The array to be heapified
+i:      The "Root" to start the heapifying on
+"""
+
+def MinHeapify(arr, i):
+    n = len(arr)
+    l = i*2+1
+    r = i*2+2
+    
+    
     return
 
-def MinHeapify():
+def BuildMinHeap(arr):
     return
--- PA2/MinQuicksort.py
+++ PA2/MinQuicksort.py
@@ -5,6 +5,11 @@ studentID: 029148637
 assignment:PA2
 '''
 
+def swapInList(A: list, x: int, y: int):
+    temp = A[x]
+    A[x] = A[y]
+    A[y] = temp
+
 """
 This is the partitioning portion, where we take a the "pivot" and put it in the right spot
 All elements left of pivot are smaller, and all elements right of pivot are bigger
@@ -20,11 +25,11 @@ def partition(A: list, p: int, r: int) -> int:
     j = p           #j is the first element after the "greater than" sub array
     #loops through the whole section being sorted
     while j < r:
-        if A[j] >= x:
-            i = i+1
-            swapInList(A, i, j)
-        j = j+1
-    swapInList(A, i+1, r)
+        if A[j] <= x:               #If current val is less than pivot
+            i = i+1                 #Add a value to less than sub array
+            swapInList(A, i, j)     #moves item into less than sub array
+        j = j+1                     #index to be looked at moves
+    swapInList(A, i+1, r)           #Moves pivot into place
     return (i+1)
 
 """
@@ -40,13 +45,15 @@ k:      The position we are looking for
 def QuickFindK(arr: list, l: int, r: int, k: int)-> int:
     n = len(arr)
     q = -1       #This will be the position of the "pivot". NOTE: The value -1 should never be returned
-    if (k >= 0) and (k < n):        #if the "pivot" is within the range of the array
+    if (k > 0) and (k <= n):        #if the "pivot" is within the range of the array
+        #print(f"partitioning from {l} to {r}")
         q = partition(arr, l, r)
+        #print(f"pivot found at {q}")
     if (q == k-1):                  #if the "pivot" is the same as the position we are looking for
         return arr[q]
-    if (q > k-1):                           #if the "pivot" is higher than the position we are looking for
+    if (q < k-1):                           #if the "pivot" is lower than the position we are looking for
         return QuickFindK(arr, q+1, r, k)
-    return QuickFindK(arr, l, q-1, k)      #if the "pivot" is lower  than the position we are looking for
+    return QuickFindK(arr, l, q-1, k)      #if the "pivot" is higher than the position we are looking for
 
 """
 This is the main calling function to find the kth Smallest element
@@ -67,5 +74,61 @@ def kthSmallest(arr: list, k: int):
         return None
     elif len(arr) == 1:
         return arr[0]
+    return QuickFindK(arr, 0, len(arr)-1, k)
 
-    return QuickFindK(arr, 0, len(arr)-1, k/)
+def testProgram():
+    arr1 = [1,2,3,4,5]
+    k1 = 3
+    arr2 = [8,6,4,2,0]
+    k2 = 2
+    arr3 = [10,6,2,4,8]
+    k3 = 5
+    arr4 = [5]
+    k4 = 1
+    arr5 = [0]
+    k5 = 0
+    
+    print("Test case 1 = ", end='')
+    print(*arr1, sep = ", ", end=' ')
+    print(f"With k value {k1}\n")
+    val = kthSmallest(arr1, k1)
+    print(f"Result = {val}\n")
+
+    print("Test case 2 = ", end='')
+    print(*arr2, sep = ", ", end=' ')
+    print(f"With k value {k2}\n")
+    val = kthSmallest(arr2, k2)
+    print(f"Result = {val}\n")
+
+    print("Test case 3 = ", end='')
+    print(*arr3, sep = ", ", end=' ')
+    print(f"With k value {k3}\n")
+    val = kthSmallest(arr3, k3)
+    print(f"Result = {val}\n")
+
+    print("Test case 4 = ", end='')
+    print(*arr4, sep = ", ", end=' ')
+    print(f"With k value {k4}\n")
+    val = kthSmallest(arr4, k4)
+    print(f"Result = {val}\n")
+
+    print("Test case 5 = ", end='')
+    print(*arr5, sep = ", ", end=' ')
+    print(f"With k value {k5}\n")
+    val = kthSmallest(arr5, k5)
+    print(f"Result = {val}\n")
+    return
+
+def simpleTest():
+    arr = [10,6,2,4,8]
+    print("Array = ",end='')
+    print(*arr, sep=", ", end="\n")
+    for i in range(1,6):
+        print(f"K = {i}")
+        val = kthSmallest(arr, i)
+        print(f"Result = {val}\n")
+    
+
+if __name__ == '__main__':
+    testProgram()
+    #simpleTest()
